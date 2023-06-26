07124a48338be8da14a162b2a9b2a603f19da3b4
unknown <maxcorrea81@outlook.com>
2023-06-26 19:49:02 -0300

    primerCommit

---
+++ src/main/java/CaC/Grupo2/FlySky/FlySkyApplication.java
@@ -0,0 +1,13 @@
+package CaC.Grupo2.FlySky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlySkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlySkyApplication.class, args);
+	}
+
+}
