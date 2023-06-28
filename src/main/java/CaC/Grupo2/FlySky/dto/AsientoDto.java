e8931a9c113aa675e56113e5d05e4106d7fa6736
Max <maxcorrea81@outlook.com>
2023-06-28 20:15:47 -0300

    Modificacion clase usuarioDTO

--- src/main/java/CaC/Grupo2/FlySky/dto/AsientoDto.java
+++ src/main/java/CaC/Grupo2/FlySky/dto/AsientoDto.java
@@ -3,6 +3,7 @@ package CaC.Grupo2.FlySky.dto;
 import javax.persistence.Column;
 
 public class AsientoDto {
+    UsuarioDto usuarioDto;
     private boolean ocupado;
     private String ubicacion;
 }
